87d66fb02a2dc7abdb381768c6b9c027b0becd32
sreejith <sreejithpillai37@gmail.com>
2026-02-25 09:44:39 +0530

    Initial commit - portfolio project

---
+++ src/constants/index.js
@@ -0,0 +1,634 @@
+import {
+  nitk,
+  cluboard,
+  cash_flow,
+  gdsc,
+  iris,
+  ecell,
+  genesis,
+  portfolio,
+  cdc,
+  chargeswap,
+  placeicon,
+  recruitment,
+  huntly,
+  oracle,
+  comicify_ai,
+  greentrust,
+  averlon,
+  devfolio,
+  pba,
+  ethglobal,
+  polkadot,
+  lightspeed,
+  dennisivy,
+  manipal,
+  icon,
+  ethforall,
+  ethindia,
+  globalAi,
+  khoj,
+  kosh,
+  polkadot_dev_cli,
+} from "../assets";
+
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillMail,
+  AiOutlineTwitter,
+  AiFillHtml5,
+  AiOutlineGitlab,
+} from "react-icons/ai";
+
+import {
+  SiDjango,
+  SiJavascript,
+  SiBootstrap,
+  SiReact,
+  SiTailwindcss,
+  SiGraphql,
+  SiPython,
+  SiCplusplus,
+  SiC,
+  SiRubyonrails,
+  SiJquery,
+  SiPostman,
+  SiGit,
+  SiMysql,
+  SiSolidity,
+  SiNetlify,
+  SiChartdotjs,
+  SiVite,
+  SiArduino,
+  SiWeb3Dotjs,
+  SiIpfs,
+  SiDotnet,
+  SiTwilio,
+  SiFlutter,
+  SiReplit,
+  SiFlask,
+  SiFigma,
+  SiGooglemaps,
+  SiOpenai,
+  SiGooglecloud,
+  SiNextdotjs,
+  SiTypescript,
+  SiClaude,
+  SiOracle,
+  SiTensorflow,
+  SiKeras,
+  SiMongodb,
+  SiApachespark,
+} from "react-icons/si";
+
+import { FaHardHat, FaRust } from "react-icons/fa";
+
+import { IoIosNotificationsOutline } from "react-icons/io";
+
+import { FaGolang } from "react-icons/fa6";
+
+import { DiCss3, DiJava, DiMsqlServer, DiRuby } from "react-icons/di";
+import { VscAzure } from "react-icons/vsc";
+import { BiLogoVisualStudio } from "react-icons/bi";
+
+export const resumeLink =
+  "https://drive.google.com/file/d/1PJCIjJbIe2TkURi9piAXv3npS5lZYZzW/view?usp=sharing";
+export const repoLink = "https://github.com/sreejithjpillai123";
+
+export const callToAction = "https://www.linkedin.com/in/sreejith-j-pillai-bb764827b/";
+
+export const navLinks = [
+  {
+    id: "skills",
+    title: "Skills & Experience",
+  },
+  {
+    id: "education",
+    title: "Education",
+  },
+
+  {
+    id: "projects",
+    title: "Projects",
+  },
+
+  {
+    id: "contactMe",
+    title: "Contact Me",
+  },
+];
+
+// Add your past academic experiences here
+export const educationList = [
+  {
+    id: "education-1",
+    icon: nitk,
+    title: "College of engineering,Attingal",
+    degree: "Bachelor of Technology",
+    duration: "December 2021 - May 2025",
+    content1: "Computer Science",
+  },
+  {
+    id: "education-2",
+    icon: pba,
+    title: "GHSS Kallar",
+    degree: "Higher Secondary Education",
+    duration: "May 2019 - June 2021",
+    content1: "Biology Science"
+  },
+  {
+    id: "education-3",
+    icon: pba,
+    title: "Vijayamatha Public School",
+    degree: "High School",
+    duration: "May 2018 - June 2019",
+    content1: "Science"
+  },
+];
+
+// Add your past achievements here for example - rankings in hackathons/events
+
+
+// Add your software developments skills here for example - programming languages, frameworks etc.
+export const skills = [
+  {
+    title: "Programming Languages",
+    items: [
+
+      {
+        id: "pl-2",
+        icon: SiPython,
+        name: "Python",
+      },
+      {
+        id: "pl-6",
+        icon: DiJava,
+        name: "Java",
+      },
+      {
+        id: "pl-8",
+        icon: AiFillHtml5,
+        name: "HTML",
+      },
+      {
+        id: "pl-9",
+        icon: DiCss3,
+        name: "CSS",
+      },
+      {
+        id: "pl-10",
+        icon: SiJavascript,
+        name: "JavaScript",
+      },
+    ],
+  },
+  {
+    title: "Frameworks/Libraries",
+    items: [
+      {
+        id: "f-1",
+        icon: SiDjango,
+        name: "Django",
+      },
+      {
+        id: "f-3",
+        icon: SiReact,
+        name: "ReactJS",
+      },
+      {
+        id: "f-4",
+        icon: SiBootstrap,
+        name: "Bootstrap",
+      },
+      {
+        id: "f-5",
+        icon: SiTailwindcss,
+        name: "Tailwind CSS",
+      },
+      {
+        id: "f-5",
+        icon: SiTailwindcss,
+        name: "Node",
+      },
+      {
+        id: "f-5",
+        icon: SiTailwindcss,
+        name: "Nextjs",
+      },
+    ],
+  },
+  {
+    title: "Tools",
+    items: [
+      {
+        id: "t-2",
+        icon: SiMysql,
+        name: "MySQL",
+      },
+      {
+        id: "t-4",
+        icon: BiLogoVisualStudio,
+        name: "VS Code",
+      },
+      {
+        id: "t-5",
+        icon: SiGit,
+        name: "Git",
+      },
+      {
+        id: "t-6",
+        icon: AiFillGithub,
+        name: "GitHub",
+      },
+      {
+        id: "t-6",
+        icon: SiMongodb,
+        name: "MongoDB",
+      },
+
+    ],
+  },
+];
+
+// Add your current/past professional work experience here
+export const experiences = [
+
+  {
+    organisation: "Expoysys Data Lab",
+    logo: averlon,
+    link: "https://averlon.ai/",
+    positions: [
+      {
+        title: "Web Developer Intern Intern",
+        duration: "Sept 2024 - oct 2024",
+        content: [
+          {
+            text: "Gained hands on experience on web developer.",
+            link: "",
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    organisation: "Qspiders",
+    logo: averlon,
+    link: "https://averlon.ai/",
+    positions: [
+      {
+        title: "Full-stack Developer Intern",
+        duration: "June 2025 - Nov 2025",
+        content: [
+          {
+            text: "Gained hands on experience on Python,ReactJS",
+            link: "",
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    organisation: "Woxro",
+    logo: averlon,
+    link: "https://averlon.ai/",
+    positions: [
+      {
+        title: "Full-stack developer intern",
+        duration: "Dec 2025 - Mar 2026",
+        content: [
+          {
+            text: "Gained hands on experience on Node,NextJS",
+            link: "",
+          },
+        ],
+      },
+    ],
+  },
+
+];
+
+// Add information about all the projects to be listed out in your portfolio
+export const projects = [
+  {
+    id: "project-1",
+    title: "AI-Cancer detection",
+    github: "https://github.com/sreejithjpillai123/cancer",
+    image: khoj,
+    content:
+      " Developed an AI-based breast cancer detection app using Flask and CNN (TensorFlow) with real-time ultrasound image uploads, predictive analytics, and a user-friendly interface.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiFlask,
+        name: "Flask",
+      },
+      {
+        id: "icon-2",
+        icon: SiTensorflow,
+        name: "Machine Learning",
+      },
+      {
+        id: "icon-3",
+        icon: SiKeras,
+        name: "CNN",
+      },
+      {
+        id: "icon-4",
+        icon: SiTailwindcss,
+        name: "CSS",
+      },
+      {
+        id: "icon-5",
+        icon: AiFillHtml5,
+        name: "HTML",
+      },
+    ],
+  },
+  {
+    id: "project-2",
+    title: "Full Stack E-commerce",
+    github: "https://github.com/sreejithjpillai123/ecommerce",
+    link: "https://ecommerce-t8kh.onrender.com",
+    image: kosh,
+    content:
+      " Built a full-stack e-commerce site using Flask, MongoDB with user login, admin panel, cart, order management, secure payments, responsive UI, and RESTful APIs.",
+    stack: [
+      {
+        id: "icon-2",
+        icon: SiTypescript,
+        name: "Flask",
+      },
+      {
+        id: "icon-2",
+        icon: SiTailwindcss,
+        name: "CSS",
+      },
+      {
+        id: "pl-2",
+        icon: SiPython,
+        name: "Python",
+      },
+      {
+        id: "pl-2",
+        icon: AiFillHtml5,
+        name: "HTML",
+      },
+      {
+        id: "pl-2",
+        icon: SiMongodb,
+        name: "MongoDB",
+      },
+    ],
+  },
+  {
+    id: "project-3",
+    title: "Story Generator",
+    github: "https://github.com/sreejithjpillai123/story-generator",
+    image: polkadot_dev_cli,
+    content:
+      "Developed a website were user can make a short story based on their keywords.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiApachespark,
+        name: "API",
+      },
+      {
+        id: "icon-1",
+        icon: AiFillHtml5,
+        name: "HTML",
+      },
+      {
+        id: "icon-1",
+        icon: SiTailwindcss,
+        name: "CSS",
+      },
+    ],
+  },
+
+  {
+    id: "project-4",
+    title: "Qitchen",
+    github: "https://github.com/sreejithjpillai123/Qitchen",
+    link: "https://qitchen-frontend-vb0f.onrender.com/",
+    image: polkadot_dev_cli,
+    content:
+      "Developed a website were user can handle a restuarant orders and reservations.",
+    stack: [
+      {
+        id: "icon-1",
+        icon: SiApachespark,
+        name: "API",
+      },
+      {
+        id: "icon-1",
+        icon: AiFillHtml5,
+        name: "HTML",
+      },
+      {
+        id: "icon-1",
+        icon: SiTailwindcss,
+        name: "CSS",
+      },
+    ],
+  },
+
+];
+
+// Add links to blogs here
+export const blogPosts = [
+  {
+    id: "post-1",
+    title: "Blog Post 01 - Title",
+    link: "#",
+    date: new Date().toLocaleDateString(), // Can be edited to any string format
+    image: "https://via.placeholder.com/600/92c952",
+    tags: [
+      {
+        id: "tag-1",
+        name: "tag 01",
+      },
+      {
+        id: "tag-2",
+        name: "tag 03",
+      },
+      {
+        id: "tag-3",
+        name: "tag 03",
+      },
+    ],
+  },
+  {
+    id: "post-2",
+    title: "Blog Post 02 - Title",
+    link: "#",
+    date: new Date().toLocaleDateString(),
+    image: "https://via.placeholder.com/600/d32776",
+    tags: [
+      {
+        id: "tag-1",
+        name: "tag 01",
+      },
+      {
+        id: "tag-2",
+        name: "tag 03",
+      },
+      {
+        id: "tag-3",
+        name: "tag 03",
+      },
+    ],
+  },
+  {
+    id: "post-3",
+    title: "Blog Post 03 - Title",
+    link: "#",
+    date: new Date().toLocaleDateString(),
+    image: "https://via.placeholder.com/600/771796",
+    tags: [
+      {
+        id: "tag-1",
+        name: "tag 01",
+      },
+      {
+        id: "tag-2",
+        name: "tag 03",
+      },
+      {
+        id: "tag-3",
+        name: "tag 03",
+      },
+    ],
+  },
+];
+
+// Highlight your GitHub stats like - Organisation, Issues Opened, Pull Requests etc.
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Organisations",
+    value: "2+",
+  },
+  {
+    id: "stats-2",
+    title: "Issues Opened",
+    value: "6+",
+  },
+  {
+    id: "stats-3",
+    title: "Pull Requests",
+    value: "6+",
+  },
+];
+
+// List out the extra curricular activities you have indulged in, like - student clubs, research groups etc.
+export const extraCurricular = [
+  {
+    id: 1,
+    organisation: "JAVA Basics from Hackerrank",
+    title: "Hackerrank",
+    content: [
+      {
+        text: "Completed the Java (Basic) certification from HackerRank, covering core concepts such as data types, loops, conditionals, classes, and object-oriented programming principles. Demonstrated proficiency through hands-on coding challenges and problem-solving tasks.",
+        link: "https://www.linkedin.com/feed/update/urn:li:activity:7097977924686942209/",
+      },
+
+    ],
+    logo: devfolio,
+  },
+  {
+    id: 2,
+    organisation: "META Introduction to Back-end development",
+    title: "META",
+    content: [
+      {
+        text: "Completed Meta’s introductory course on back-end development, covering core concepts such as server-side programming, databases, APIs, and version control (Git/GitHub). Gained practical knowledge in setting up servers, handling requests, and understanding how websites function behind the scenes.",
+        link: "",
+      },
+    ],
+    logo: gdsc,
+  },
+  {
+    id: 3,
+    organisation: " Data through Art of visualization",
+    title: "Coursera",
+    content: [
+      {
+        text: "Completed a course focused on presenting data effectively using visual tools and design principles. Learned to communicate insights through charts, graphs, and dashboards using tools like Tableau and Excel, with an emphasis on clarity, storytelling, and user-focused design.",
+        link: "",
+      },
+    ],
+    logo: genesis,
+  },
+  {
+    id: 4,
+    organisation: " Qspiders",
+    title: "Qspiders",
+    content: [
+      {
+        text: "Completed a comprehensive Full Stack Development course at QSpiders with a focus on building dynamic and scalable web applications. Gained hands-on experience in Python, Django, REST APIs, and frontend development using React, HTML, CSS, and JavaScript. Learned database management with MySQL, version control using Git, and deployment fundamentals.",
+        link: "",
+      },
+    ],
+    logo: genesis,
+  },
+  {
+    id: 4,
+    organisation: " Woxro",
+    title: "Woxro",
+    content: [
+      {
+        text: "Completed a Full Stack Development program at Woxro with a focus on building modern. Gained practical experience in Node.js, Express.js, and MongoDB for backend development, along with frontend development using Next.js, React, HTML, and CSS. Developed RESTful APIs, implemented authentication and CRUD operations.",
+        link: "",
+      },
+    ],
+    logo: genesis,
+  },
+];
+
+// Links to your social media profiles
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: AiFillLinkedin,
+    link: "https://www.linkedin.com/in/sreejith-j-pillai-bb764827b/",
+  },
+  {
+    id: "social-media-2",
+    icon: AiFillGithub,
+    link: "https://github.com/sreejithjpillai123",
+  },
+  {
+    id: "social-media-3",
+    icon: AiFillMail,
+    link: "mailto:sreejithpillai37@gmail.com",
+  },
+  {
+    id: "social-media-5",
+    icon: AiFillInstagram,
+    link: "https://www.instagram.com/_sreejith_pillai",
+  },
+];
+
+// Your professional summary
+export const aboutMe = {
+  name: "Sreejith J Pillai",
+  githubUsername: "mittal-parth",
+
+  intro:
+    "Software Developer from India who is either busy improving his craft or pondering over the next big idea.",
+};
+
+// The maximum number of PRs to be displayed in the Open Source Contributions section.
+export const itemsToFetch = 20;
+
+// Add names of GitHub repos you'd like to display open source contributions from in the 'org/repo' format.
+export const includedRepos = [
+  "publiclab/plots2",
+  "zulip/zulip",
+  "paritytech/polkadot-sdk",
+];
